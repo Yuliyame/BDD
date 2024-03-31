0cacb964c37929f72d6e2e354e9da92344619005
YuliyaYuliya <yr555556666667777777@gmail.com>
2024-03-31 16:28:14 +0300

    login page

--- src/test/java/ru.netology.web/test/MoneyTransferTest.java
+++ src/test/java/ru.netology.web/test/MoneyTransferTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.*;
-
 public class MoneyTransferTest {
     DashboardPage dashboardPage;
     CardInfo firstCardInfo;
